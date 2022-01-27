cd3328998cb10af55d3aedbd0ebdd9dae85e672b
ATMECS\Prashanth.Chinala <prashanth.chinala@atmecs.com>
2022-01-27 23:03:00 +0530

    voice call webhook

--- src/index.js
+++ src/index.js
@@ -3,58 +3,66 @@ const express=require('express')
 const app =express();
 const axios=require('axios');
 const dotenv = require('dotenv');
+const twilio = require('twilio');
 dotenv.config();
 app.set('view engine','hbs')
 
-app.get('',(req,res)=>{
-    // setInterval(fetchOrders,3000)
-    // console.log(res)
-    res.render('index')
+app.get('/voicecall',()=>{
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = new twilio(accountSid, authToken);
+    client.calls
+          .create({
+             url: 'Trading View Alert Triggered',
+             to: '+919912202122',
+             from: '+19302122122'
+           })
+          .then(call => console.log(call.sid));
 })
 
-app.get('/dashboard',(req,res)=>{
-    res.send("Dashboard Page")
-})
+// app.get('/voicecall',(req,res)=>{
+
+// })
 
 app.listen(3000,()=>{
     console.log(`server listening at port 3000`)
 })
 
-const fetchOrders= async()=>{
+const fetchPrice= async()=>{
     try{
+    let isBought=false;
+    let isSold=false;
     const result=await axios.get(`${process.env.API_BASE_URL}${process.env.API_TICKER_INFO}${process.env.TICKER_NAME}`)
     let currentPrice= Number(result.data.lastPrice);
     let buyPrice= Number(process.env.TICKER_BUY_PRICE)
+    let sellPrice=Number(process.env.TICKER_SELL_PRICE)
     console.log(currentPrice)
-    if(buyPrice  <= currentPrice){
-        console.log("BUY")
+    if(currentPrice  <= buyPrice){
+        console.log(`BUY ${process.env.TICKER_NAME} at ${currentPrice}`)
     }
-    // console.log(result.data)
+    if(currentPrice >= sellPrice){
+        console.log(`Selling ${process.env.TICKER_NAME} at ${currentPrice}`)
+    }
     }
     catch(err){
         console.log(err)
     }
 }
-const cancelExistingOrders= async()=>{
+
+// const activeOrders =async ()=>{
+
+// },
+
+// const cancelExistingOrders= async()=>{
     
-}
-const placeNewOrder= async()=>{
+// },
+// const placeNewOrder= async()=>{
     
-}
-const isOrderFilled= async()=>{
+// },
+// const isOrderFilled= async()=>{
     
-}
+// }
 
-fetchOrders();
+// fetchPrice();
 
-// setInterval(fetchOrders,5000)
-
-
-
-
-
-
-
-
-
-// run();
+// setInterval(fetchPrice,process.env.SET_INTERVAL);
