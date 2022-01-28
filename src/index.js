281c01783ed52b54efbfb9aeb875ea874937665e
ATMECS\Prashanth.Chinala <prashanth.chinala@atmecs.com>
2022-01-28 09:57:32 +0530

    rollback old code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ const twilio = require('twilio');
 dotenv.config();
 app.set('view engine','hbs')
 
-app.get('/voicecall',()=>{
+
+app.get('',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/voicecall',(req,res)=>{
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = new twilio(accountSid, authToken);
@@ -17,37 +22,39 @@ app.get('/voicecall',()=>{
              to: '+919912202122',
              from: '+19302122122'
            })
-          .then(call => console.log(call.sid));
+          .then((call)=>{
+            console.log(call.sid)
+            res.send("Calling")
+          })
+        //   (call => console.log(call.sid));
 })
 
-// app.get('/voicecall',(req,res)=>{
 
-// })
 
 app.listen(3000,()=>{
     console.log(`server listening at port 3000`)
 })
 
-const fetchPrice= async()=>{
-    try{
-    let isBought=false;
-    let isSold=false;
-    const result=await axios.get(`${process.env.API_BASE_URL}${process.env.API_TICKER_INFO}${process.env.TICKER_NAME}`)
-    let currentPrice= Number(result.data.lastPrice);
-    let buyPrice= Number(process.env.TICKER_BUY_PRICE)
-    let sellPrice=Number(process.env.TICKER_SELL_PRICE)
-    console.log(currentPrice)
-    if(currentPrice  <= buyPrice){
-        console.log(`BUY ${process.env.TICKER_NAME} at ${currentPrice}`)
-    }
-    if(currentPrice >= sellPrice){
-        console.log(`Selling ${process.env.TICKER_NAME} at ${currentPrice}`)
-    }
-    }
-    catch(err){
-        console.log(err)
-    }
-}
+// const fetchPrice= async()=>{
+//     try{
+//     let isBought=false;
+//     let isSold=false;
+//     const result=await axios.get(`${process.env.API_BASE_URL}${process.env.API_TICKER_INFO}${process.env.TICKER_NAME}`)
+//     let currentPrice= Number(result.data.lastPrice);
+//     let buyPrice= Number(process.env.TICKER_BUY_PRICE)
+//     let sellPrice=Number(process.env.TICKER_SELL_PRICE)
+//     console.log(currentPrice)
+//     if(currentPrice  <= buyPrice){
+//         console.log(`BUY ${process.env.TICKER_NAME} at ${currentPrice}`)
+//     }
+//     if(currentPrice >= sellPrice){
+//         console.log(`Selling ${process.env.TICKER_NAME} at ${currentPrice}`)
+//     }
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
 
 // const activeOrders =async ()=>{
 
